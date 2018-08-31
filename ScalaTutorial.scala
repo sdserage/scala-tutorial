b581e281d9961673e0d4c18aff5e9c8180b2a09c
Scott Serage <xttocsurge51@gmail.com>
2018-08-31 16:17:15 -0600

    Initial commit

---
+++ ScalaTutorial.scala
@@ -0,0 +1,19 @@
+object ScalaTutorial {
+  def main(args: Array[String]) {
+    var i = 0
+
+    // Loop while
+
+    // while(i <= 10){
+    //   println(i)
+    //   i += 1
+    // }
+
+    // do loop
+
+    do {
+      println(i)
+      i += 1
+    } while (i <=20)
+  }
+}
