0e827df7287e968f24ae5633c248b2e8671ea466
Scott Serage <xttocsurge51@gmail.com>
2018-09-05 10:57:35 -0600

    Tested several ways to use and create higher order functions

--- ScalaTutorial.scala
+++ ScalaTutorial.scala
@@ -312,31 +312,61 @@ object ScalaTutorial {
 
     /* Traits */
 
-    val superman = new Superhero("Superman")
-    println(superman.fly)
-    println(superman.hitByBullet)
-    println(superman.ricochet(2500))
+    // val superman = new Superhero("Superman")
+    // println(superman.fly)
+    // println(superman.hitByBullet)
+    // println(superman.ricochet(2500))
+
+    /* Higher Order Functions */
+
+    // Store log10 as a variable and then call it using the created variable
+    val log10Func = log10 _
+    println(log10Func(1000))
+
+    // Map over a list and perform a function (and then print the result)
+    List(1000.0, 10000.0).map(log10Func).foreach(println)
+
+    // Multiply the input by 50 for each item in the list
+    List(1,2,3,4,5).map((x: Int) => x * 50).foreach(println)
+
+    // Filter example
+    List(1,2,3,4,5).filter(_ % 2 == 0).foreach(println)
+
+    // Functions to be passed in
+    def times3(num: Int) = num * 3
+    def times4(num: Int) = num * 4
+
+    // Takes a function that returns and Int and changes it to a double,
+    // also takes an Int to be passed into the function it recieves
+    def multIt(func: (Int) => Double, num: Int) = {
+      func(num)
+    }
+    printf("4 * 100 = %.1f\n", multIt(times4, 100))
 
   } // END OF MAIN
 
+  // Higher Order Functions
+
+
+
   // Traits
 
-  trait Flyable {
-    def fly: String
-  }
+  // trait Flyable {
+  //   def fly: String
+  // }
 
-  trait BulletProof {
-    def hitByBullet: String
-    def ricochet(startSpeed: Double): String = {
-      "The bullet ricochets at a speed of %.1f ft/sec".format(startSpeed * .75)
-    }
-  }
+  // trait BulletProof {
+  //   def hitByBullet: String
+  //   def ricochet(startSpeed: Double): String = {
+  //     "The bullet ricochets at a speed of %.1f ft/sec".format(startSpeed * .75)
+  //   }
+  // }
 
-  class Superhero(val name: String) extends Flyable with BulletProof {
-    def fly = "%s flys through the air".format(this.name)
+  // class Superhero(val name: String) extends Flyable with BulletProof {
+  //   def fly = "%s flys through the air".format(this.name)
 
-    def hitByBullet = "The bullet bounces off of %s".format(this.name)
-  }
+  //   def hitByBullet = "The bullet bounces off of %s".format(this.name)
+  // }
 
   // Abstract Classes
 
