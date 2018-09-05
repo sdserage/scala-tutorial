ca9644fae24782a8f80eaea019f616ca17d23df1
Scott Serage <xttocsurge51@gmail.com>
2018-09-05 09:45:44 -0600

    Created the abstract class 'Mammal' and the 'Wolf' Class that inherits from it
    Learned about abstract classes.

--- ScalaTutorial.scala
+++ ScalaTutorial.scala
@@ -285,79 +285,99 @@ object ScalaTutorial {
 
     /* Classes */
 
-    // Create an animal and then set its values
-    val rover = new Animal
-    rover.setName("Rover")
-    rover.setSound("Woof")
-    printf("%s says %s\n", rover.getName, rover.getSound)
+    // // Create an animal and then set its values
+    // val rover = new Animal
+    // rover.setName("Rover")
+    // rover.setSound("Woof")
+    // printf("%s says %s\n", rover.getName, rover.getSound)
 
-    // Create an animal with values passed in to begin with
-    val whiskers = new Animal("Whiskers", "Meow")
-    println(s"${whiskers.getName} with id ${whiskers.id} says ${whiskers.sound}")
+    // // Create an animal with values passed in to begin with
+    // val whiskers = new Animal("Whiskers", "Meow")
+    // println(s"${whiskers.getName} with id ${whiskers.id} says ${whiskers.sound}")
 
-    // Use overridden toString
-    println(whiskers.toString)
+    // // Use overridden toString
+    // println(whiskers.toString)
 
-    /* Inheritance */
+    // // Inheritance
 
-    val spike = new Dog("Spike", "Woof", "Grrrrr")
-    spike.setName("Spike")
-    println(spike.toString)
+    // val spike = new Dog("Spike", "Woof", "Grrrrr")
+    // spike.setName("Spike")
+    // println(spike.toString)
+
+    /* Abstract Classes */
+
+    val fang = new Wolf("Fang")
+    fang.moveSpeed = 36.0
+    println(fang.move)
 
   } // END OF MAIN
 
-  class Animal(var name: String, var sound: String){
-    // Default contructor
-    this.setName(name)
-    val id = Animal.newIdNum
+  // Abstract Classes
 
-    def getName(): String = name
-    def getSound(): String = sound
-    def setName(name: String){
-      if(!(name.matches(".*\\d+.*")))
-        this.name = name
-      else
-        this.name = "No Name"
-    }
-    def setSound(sound: String){
-      this.sound = sound
-    }
-    // Constructor with only name passed in
-    def this(name: String){
-      this("No Name", "No Sound")
-      this.setName(name)
-      this.setSound(sound)
-    }
-    // Constructor with no name or sound passed in
-    def this(){
-      this("No Name", "No Sound")
-    }
-    // Override toString
-    override def toString(): String = {
-      "%s with the id %d says %s".format(this.name, this.id, this.sound)
-    }
-  }
-  // Companion object (Notice that it is outside of the Animal class)
-  object Animal {
-    private var idNumber = 0
-    private def newIdNum = {idNumber += 1; idNumber}
+  abstract class Mammal(val name: String){
+    var moveSpeed: Double
+    def move: String
   }
 
-  class Dog(name: String, sound: String, growl: String) extends Animal(name, sound){
-    def this(name: String, sound: String){
-      this("No Name", sound, "No Growl")
-      this.setName(name)
-    }
-    def this(name: String){
-      this("No Name", "No Sound", "No Growl")
-      this.setName(name)
-    }
-    def this(){
-      this("No Name", "No Sound", "No Growl")
-    }
-    override def toString(): String = {
-      "%s with the id %d says %s or %s".format(this.name, this.id, this.sound, this.growl)
-    }
+  class Wolf(name: String) extends Mammal(name){
+    var moveSpeed = 35.0
+    def move = "The wolf %s runs %.2f mph".format(this.name, this.moveSpeed)
   }
+
+  // Classes and Inheritance
+
+  // class Animal(var name: String, var sound: String){
+  //   // Default contructor
+  //   this.setName(name)
+  //   val id = Animal.newIdNum
+
+  //   def getName(): String = name
+  //   def getSound(): String = sound
+  //   def setName(name: String){
+  //     if(!(name.matches(".*\\d+.*")))
+  //       this.name = name
+  //     else
+  //       this.name = "No Name"
+  //   }
+  //   def setSound(sound: String){
+  //     this.sound = sound
+  //   }
+  //   // Constructor with only name passed in
+  //   def this(name: String){
+  //     this("No Name", "No Sound")
+  //     this.setName(name)
+  //     this.setSound(sound)
+  //   }
+  //   // Constructor with no name or sound passed in
+  //   def this(){
+  //     this("No Name", "No Sound")
+  //   }
+  //   // Override toString
+  //   override def toString(): String = {
+  //     "%s with the id %d says %s".format(this.name, this.id, this.sound)
+  //   }
+  // }
+  // // Companion object (Notice that it is outside of the Animal class)
+  // object Animal {
+  //   private var idNumber = 0
+  //   private def newIdNum = {idNumber += 1; idNumber}
+  // }
+
+  // class Dog(name: String, sound: String, growl: String) extends Animal(name, sound){
+  //   def this(name: String, sound: String){
+  //     this("No Name", sound, "No Growl")
+  //     this.setName(name)
+  //   }
+  //   def this(name: String){
+  //     this("No Name", "No Sound", "No Growl")
+  //     this.setName(name)
+  //   }
+  //   def this(){
+  //     this("No Name", "No Sound", "No Growl")
+  //   }
+  //   override def toString(): String = {
+  //     "%s with the id %d says %s or %s".format(this.name, this.id, this.sound, this.growl)
+  //   }
+  // }
 
 } // END OF ScalaTutorial
